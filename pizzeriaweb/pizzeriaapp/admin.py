51bc2454ecaddf92801b23de5e3d2b6dc83e7f2b
Raul <rulo.marsan23@gmail.com>
2021-05-18 11:56:44 +0200

    Crear admins, hacer migraciones, crear superuser

--- pizzeriaweb/pizzeriaapp/admin.py
+++ pizzeriaweb/pizzeriaapp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Pizza, Complemento
 
-# Register your models here.
+@admin.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+
+    list_display = ['nombre', 'ingredientes', 'updated']
+    list_filter = ['nombre', 'ingredientes']
+    fields = ('nombre', 'ingredientes', 'foto')    
+    search_fields = ('nombre', 'ingredientes')
+    
+
+    def get_queryset(self, request):
+        return super(PizzaAdmin, self).get_queryset(request).order_by('-updated')
+
+@admin.register(Complemento)
+class ComplementoAdmin(admin.ModelAdmin):
+
+    list_display = ['nombre', 'descripcion', 'updated']
+    list_filter = ['nombre', 'descripcion']
+    fields = ('nombre', 'descripcion', 'foto')    
+    search_fields = ('nombre', 'descripcion')
+    
+
+    def get_queryset(self, request):
+        return super(ComplementoAdmin, self).get_queryset(request).order_by('-updated')
