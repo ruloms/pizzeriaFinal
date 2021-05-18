9552e149de668dc460eaf9d5b274ef403fc9bbb1
Raul <rulo.marsan23@gmail.com>
2021-05-18 10:51:10 +0200

    Modelos creados, con la carpeta de imágenes y los settings y urls pertinentes.

--- pizzeriaweb/pizzeriaapp/models.py
+++ pizzeriaweb/pizzeriaapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Pizza(models.Model):
+    nombre = models.CharField(max_length=25, db_Index=True)
+    ingredientes = models.CharField(max_length=100, db_Index=True)
+    foto = models.ImageField(upload_to='pizzas/%Y/%m/%d', default='pizzas/pizzadef.jpg')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return(self.nombre)
+
+class Complemento(models.Model):
+    nombre = models.CharField(max_length=25, db_Index=True)
+    descripcion = models.CharField(max_length=100)
+    foto = models.ImageField(upload_to='complementos/%Y/%m/%d', default='complementos/complementodef.png')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return(self.nombre)
+
